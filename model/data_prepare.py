c1168b4fa342e00cdca782a8d23b54a21cace013
xjtu-BeiWu <xjtu_BeiWu@163.com>
2019-11-14 14:49:45 +0800

    upgrade data prepare processing

--- model/data_prepare.py
+++ model/data_prepare.py
@@ -32,18 +32,18 @@ def extract_labels(filename, num_patents, d_label):
 
 
 if __name__ == '__main__':
-    input_file_path = ''
-    input_label_path = ''
-    output_file_path = ''
-    output_label_path = ''
-    num_patent = 0
-    dim_features = 0
-    dim_label = 0
+    input_file_path = '/home/wubei/data_100_5/all_vector.txt'
+    input_label_path = '/home/wubei/data_100_5/class_vector.txt'
+    output_file_path = '/home/wubei/data_100_5_npy/all_vector.npy'
+    output_label_path = '/home/wubei/data_100_5_npy/class_vector.npy'
+    num_patent = 1807188
+    dim_feature = 359
+    dim_label = 629
     start_time = time.time()
     print("Start transfer data!")
-    features = extract_data(input_file_path, num_patent, dim_features)
-    feature_file = np.save(output_file_path, features)
+    feature = extract_data(input_file_path, num_patent, dim_feature)
+    feature_file = np.save(output_file_path, feature)
     labels = extract_labels(input_label_path, num_patent, dim_label)
-    label_file = np.save(output_label_path)
+    label_file = np.save(output_label_path, labels)
     elapsed_time = time.time() - start_time
     print(elapsed_time, 's')
